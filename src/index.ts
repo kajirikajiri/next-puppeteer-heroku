5d6c4a5848538e74a7fa9fa74c566cb10260d648
kajirikajiri <kajirikajiri@gmail.com>
2021-01-29 11:54:27 +0900

    :art:

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,22 @@
 import express from 'express'
 import { domEventCreate, domEventIndex, root } from './routes';
+import cors from 'cors'
 
 const app: express.Express = express()
 app.set('port', (process.env.PORT || 4000));
 // CORSの許可
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "chrome-extension://jhildnafefadjmkaodnlooikgokogkld")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next()
-})
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "chrome-extension://jhildnafefadjmkaodnlooikgokogkld")
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+//   next()
+// })
 // body-parserに基づいた着信リクエストの解析
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 // GetとPostのルーティング
 // const router: express.Router = express.Router()
 
-app.get('/api/health', (req:express.Request, res:express.Response) => {
+app.get('/api/health', cors({origin: /chrome-extension/}), (req:express.Request, res:express.Response) => {
   res.send('hi !!')
 })
 
